4bb9495c251a5de924f49924f63a77b43bfa3f50
Harika <17211a05h0@bvrit.ac.in>
2020-07-26 11:04:46 +0530

    Calculator

---
+++ src/main/java/com/epam/DesignPrinciples/App.java
@@ -0,0 +1,31 @@
+package com.epam.DesignPrinciples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /*public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }*/
+	
+		public int addition(int a,int b)
+		{
+			return a+b;
+		}
+		public  int subtraction(int a,int b)
+		{
+			return a-b;
+		}
+		public int multiplication(int a,int b)
+		{
+			return a*b;
+		}
+		public int division(int a,int b)
+		{
+			return a/b;
+		}
+	
+}
